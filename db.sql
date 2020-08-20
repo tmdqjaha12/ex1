74a9de777d5f1a3fc9e98e61cf93ccc680fbaa84
tmdqjaha12 <tmdqjaha12@naver.com>
2020-08-20 09:25:42 +0900

    db.slq파일 추가

---
+++ db.sql
@@ -0,0 +1,62 @@
+# 캐릭터SET 설정
+SET NAMES utf8mb4;
+
+# DB 생성
+DROP DATABASE IF EXISTS ex1;
+CREATE DATABASE ex1;
+USE ex1;
+
+# 회원 테이블 생성
+DROP TABLE IF EXISTS `member`;
+CREATE TABLE `member` (
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    `loginId` CHAR(100) NOT NULL UNIQUE,
+    `loginPw` CHAR(100) NOT NULL,
+    `name` CHAR(100) NOT NULL, 
+    `nickname` CHAR(100) NOT NULL UNIQUE,
+    `email` CHAR(100) NOT NULL,
+    `cellphoneNo` CHAR(30) NOT NULL,
+    `level` INT(1) UNSIGNED DEFAULT 0 NOT NULL,
+    `delStatus` INT(1) UNSIGNED DEFAULT 0 NOT NULL
+);
+
+# 마스터 회원 생성
+INSERT INTO `member` SET
+regDate = NOW(),
+updateDate = NOW(),
+`loginId` = 'admin',
+`loginPw` = SHA2('admin', 256),
+`name` = 'admin',
+`nickname` = 'admin',
+`email` = 'admin@admin.com',
+`cellphoneNo` = '',
+`level` = 10;
+
+# 부가정보테이블 
+DROP TABLE IF EXISTS attr;
+CREATE TABLE attr (
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    `name` CHAR(100) NOT NULL UNIQUE,
+    `value` TEXT NOT NULL
+);
+
+# attr 테이블에서 name 을 4가지 칼럼으로 나누기
+ALTER TABLE `attr` DROP COLUMN `name`,
+ADD COLUMN `relTypeCode` CHAR(20) NOT NULL AFTER `updateDate`,
+ADD COLUMN `relId` INT(10) UNSIGNED NOT NULL AFTER `relTypeCode`,
+ADD COLUMN `typeCode` CHAR(30) NOT NULL AFTER `relId`,
+ADD COLUMN `type2Code` CHAR(30) NOT NULL AFTER `typeCode`,
+CHANGE `value` `value` TEXT CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL AFTER `type2Code`,
+DROP INDEX `name`; 
+
+# attr 유니크 인덱스 걸기
+## 중복변수 생성금지
+## 변수찾는 속도 최적화
+ALTER TABLE `attr` ADD UNIQUE INDEX (`relTypeCode`, `relId`, `typeCode`, `type2Code`); 
+
+## 특정 조건을 만족하는 회원 또는 게시물(기타 데이터)를 빠르게 찾기 위해서
+ALTER TABLE `attr` ADD INDEX (`relTypeCode`, `typeCode`, `type2Code`); 
